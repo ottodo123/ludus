0f3e056204dce17597dfbec08b77054405347b5a
ottodo123 <otto@blind.com>
2025-06-20 02:01:48 -0400

    d

--- ludus-flashcards/src/App.js
+++ ludus-flashcards/src/App.js
@@ -7,14 +7,43 @@ import LudusFolder from './components/LudusFolder';
 import FlashcardsPage from './components/FlashcardsPage';
 import GlossaryPage from './components/GlossaryPage';
 import GrammarPage from './components/GrammarPage';
+import StudySession from './components/StudySession';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('ludus');
+  const [studyCards, setStudyCards] = useState([]);
+  const [isStudying, setIsStudying] = useState(false);
+
+  const handleStartStudy = (cards) => {
+    setStudyCards(cards);
+    setIsStudying(true);
+  };
+
+  const handleBackToPage = () => {
+    setIsStudying(false);
+    setStudyCards([]);
+  };
 
   const renderPage = () => {
+    // If in study mode, show study session regardless of current page
+    if (isStudying) {
+      return (
+        <StudySession
+          cards={studyCards}
+          onComplete={handleBackToPage}
+          onBack={handleBackToPage}
+        />
+      );
+    }
+
     switch(currentPage) {
       case 'ludus':
-        return <LudusFolder onPageChange={setCurrentPage} />;
+        return (
+          <LudusFolder 
+            onBack={() => setCurrentPage('flashcards')}
+            onStartStudy={handleStartStudy}
+          />
+        );
       case 'flashcards':
         return <FlashcardsPage />;
       case 'glossary':
@@ -22,7 +51,12 @@ function App() {
       case 'grammar':
         return <GrammarPage />;
       default:
-        return <LudusFolder onPageChange={setCurrentPage} />;
+        return (
+          <LudusFolder 
+            onBack={() => setCurrentPage('flashcards')}
+            onStartStudy={handleStartStudy}
+          />
+        );
     }
   };
 
